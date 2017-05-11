8d4bf60e6f688a82bf91d363a876427a726b4d4c
Max Albrecht <max.albrecht@zhdk.ch>
2017-05-11 11:42:38 +0200

    test

--- @eins78/rspec-render-docs/src/ui/Markdown.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-
-// inline-only markdown
-export const Md = ({children}) => (
-  <ReactMarkdown source={children} escapeHtml
-    unwrapDisallowed allowedTypes={['Text', 'Strong', 'Emph', 'Code']} />
-)
