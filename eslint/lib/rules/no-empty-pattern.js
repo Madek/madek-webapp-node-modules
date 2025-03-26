1bd541c3f8bce84fda532d10b4beb0896d6f4dba
Jan Pistor <pistor.jan@gmail.com>
2025-03-26 18:12:32 +0100

    chore: update eslint to v9 and migrate eslint config

--- eslint/lib/rules/no-empty-pattern.js
+++ eslint/lib/rules/no-empty-pattern.js
@@ -12,67 +12,74 @@ const astUtils = require("./utils/ast-utils");
 
 /** @type {import('../shared/types').Rule} */
 module.exports = {
-    meta: {
-        type: "problem",
+	meta: {
+		type: "problem",
 
-        docs: {
-            description: "Disallow empty destructuring patterns",
-            recommended: true,
-            url: "https://eslint.org/docs/latest/rules/no-empty-pattern"
-        },
+		defaultOptions: [
+			{
+				allowObjectPatternsAsParameters: false,
+			},
+		],
 
-        schema: [
-            {
-                type: "object",
-                properties: {
-                    allowObjectPatternsAsParameters: {
-                        type: "boolean",
-                        default: false
-                    }
-                },
-                additionalProperties: false
-            }
-        ],
+		docs: {
+			description: "Disallow empty destructuring patterns",
+			recommended: true,
+			url: "https://eslint.org/docs/latest/rules/no-empty-pattern",
+		},
 
-        messages: {
-            unexpected: "Unexpected empty {{type}} pattern."
-        }
-    },
+		schema: [
+			{
+				type: "object",
+				properties: {
+					allowObjectPatternsAsParameters: {
+						type: "boolean",
+					},
+				},
+				additionalProperties: false,
+			},
+		],
 
-    create(context) {
-        const options = context.options[0] || {},
-            allowObjectPatternsAsParameters = options.allowObjectPatternsAsParameters || false;
+		messages: {
+			unexpected: "Unexpected empty {{type}} pattern.",
+		},
+	},
 
-        return {
-            ObjectPattern(node) {
+	create(context) {
+		const [{ allowObjectPatternsAsParameters }] = context.options;
 
-                if (node.properties.length > 0) {
-                    return;
-                }
+		return {
+			ObjectPattern(node) {
+				if (node.properties.length > 0) {
+					return;
+				}
 
-                // Allow {} and {} = {} empty object patterns as parameters when allowObjectPatternsAsParameters is true
-                if (
-                    allowObjectPatternsAsParameters &&
-                    (
-                        astUtils.isFunction(node.parent) ||
-                        (
-                            node.parent.type === "AssignmentPattern" &&
-                            astUtils.isFunction(node.parent.parent) &&
-                            node.parent.right.type === "ObjectExpression" &&
-                            node.parent.right.properties.length === 0
-                        )
-                    )
-                ) {
-                    return;
-                }
+				// Allow {} and {} = {} empty object patterns as parameters when allowObjectPatternsAsParameters is true
+				if (
+					allowObjectPatternsAsParameters &&
+					(astUtils.isFunction(node.parent) ||
+						(node.parent.type === "AssignmentPattern" &&
+							astUtils.isFunction(node.parent.parent) &&
+							node.parent.right.type === "ObjectExpression" &&
+							node.parent.right.properties.length === 0))
+				) {
+					return;
+				}
 
-                context.report({ node, messageId: "unexpected", data: { type: "object" } });
-            },
-            ArrayPattern(node) {
-                if (node.elements.length === 0) {
-                    context.report({ node, messageId: "unexpected", data: { type: "array" } });
-                }
-            }
-        };
-    }
+				context.report({
+					node,
+					messageId: "unexpected",
+					data: { type: "object" },
+				});
+			},
+			ArrayPattern(node) {
+				if (node.elements.length === 0) {
+					context.report({
+						node,
+						messageId: "unexpected",
+						data: { type: "array" },
+					});
+				}
+			},
+		};
+	},
 };
